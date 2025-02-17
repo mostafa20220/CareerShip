d07147bc72f3a321e13fb43806f8b3d05006c9ac
Omar <omar1752003@gmail.com>
2025-02-17 16:30:04 +0200

    updated "Task" model

--- projects/models.py
+++ projects/models.py
@@ -1,16 +1,27 @@
 from django.db import models
 
+
 class DifficultyLevel(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True, null=True)
+
 
 class Category(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True, null=True)
 
+
 class Project(models.Model):
-    difficulty_level = models.ForeignKey('DifficultyLevel', on_delete=models.SET_NULL, null=True, blank=True, db_index=True)
-    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=True, db_index=True)
+    difficulty_level = models.ForeignKey(
+        'DifficultyLevel',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        db_index=True,
+    )
+    category = models.ForeignKey(
+        'Category', on_delete=models.SET_NULL, null=True, blank=True, db_index=True
+    )
     name = models.CharField(max_length=255)
     description = models.TextField()
     slug = models.SlugField(unique=True)
@@ -18,20 +29,35 @@ class Project(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     max_team_size = models.PositiveSmallIntegerField()
 
+
 class Task(models.Model):
-    project = models.ForeignKey('Project', on_delete=models.CASCADE, db_index=True)
-    difficulty_level = models.ForeignKey('DifficultyLevel', on_delete=models.SET_NULL, null=True, blank=True, db_index=True)
+    project = models.ForeignKey(
+        'Project', on_delete=models.CASCADE, db_index=True, related_name="tasks"
+    )
+    difficulty_level = models.ForeignKey(
+        'DifficultyLevel',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        db_index=True,
+    )
     name = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
     duration = models.TimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
 
 class Prerequisite(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField()
 
+
 class TaskPrerequisite(models.Model):
     task = models.ForeignKey('Task', on_delete=models.CASCADE, db_index=True)
-    prerequisite = models.ForeignKey('Prerequisite', on_delete=models.CASCADE, db_index=True)
+    prerequisite = models.ForeignKey(
+        'Prerequisite', on_delete=models.CASCADE, db_index=True
+    )
+
 
 class Endpoint(models.Model):
     task = models.ForeignKey('Task', on_delete=models.CASCADE, db_index=True)
@@ -39,13 +65,16 @@ class Endpoint(models.Model):
     path = models.TextField()
     description = models.TextField(blank=True, null=True)
 
+
 class Submission(models.Model):
     task = models.ForeignKey('Task', on_delete=models.CASCADE, db_index=True)
     user = models.ForeignKey('users.User', on_delete=models.CASCADE, db_index=True)
-    team = models.ForeignKey('teams.Team', on_delete=models.CASCADE, null=True, blank=True, db_index=True)
+    team = models.ForeignKey(
+        'teams.Team', on_delete=models.CASCADE, null=True, blank=True, db_index=True
+    )
     is_pass = models.BooleanField()
     feedback = models.TextField(blank=True, null=True)
-    #TODO: one of the following fields should be filled based on the project track or field.
+    # TODO: one of the following fields should be filled based on the project track or field.
     deployment_url = models.URLField(null=True, blank=True)
     github_url = models.URLField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
