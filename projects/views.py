b240b2773da49d971f8ee41ec9f1d55ebcce9984
Omar <omar1752003@gmail.com>
2025-02-17 16:07:59 +0200

    created an endpoint that lists all categories

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from .models import *
+from .serializers import CategorySerializer
+
+import traceback
+
+
+@api_view(["GET"])
+def list_categories(request):
+    try:
+        categories = Category.objects.all()
+        data = CategorySerializer(categories, many=True).data
+
+        return Response(data, status=status.HTTP_200_OK)
+    except:
+        print(traceback.format_exc())
+        return Response(
+            {"error": "Something went wrong."},
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
