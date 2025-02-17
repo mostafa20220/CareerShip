27dbf8447cff4141a7242d2dac2ea06f83446d63
mohamed hesham <scaret2tv@gmail.com>
2025-02-17 21:26:15 +0200

    certificate pdf generation logic

--- CareerShip/urls.py
+++ CareerShip/urls.py
@@ -17,9 +17,16 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+import certificates.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('api/v1/' , include([
+        path('certificates/', certificates.views.generate_certificate , name='generate_certificate'),
+    ]) ),
+
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
