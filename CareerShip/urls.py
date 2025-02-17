326a47d4557dda5d1914af5042f96d21380f1a4b
Omar <omar1752003@gmail.com>
2025-02-17 20:41:55 +0200

    followed REST naming conventions

--- CareerShip/urls.py
+++ CareerShip/urls.py
@@ -22,7 +22,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/projects/', include('projects.urls')),
+    path('api/v1/projects/', include('projects.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(
     settings.STATIC_URL, document_root=settings.STATIC_ROOT
--- projects/urls.py
+++ projects/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('list-categories/', list_categories, name='list-categories'),
-    path('list-projects/', list_projects, name='list-projects'),
-    path('get-project/<str:pk>/', get_project, name='get-project'),
-    path('submit-task/', submit_task, name='submit-task'),
+    path("categories/", list_categories, name="categories-list"),
+    path("", list_projects, name="projects-list"),
+    path("<int:pk>/", get_project, name="get-project"),
+    path("submit-task/", submit_task, name="submit-task"),
 ]
