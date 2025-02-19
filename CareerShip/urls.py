1f4c3e75903d82dbe2f09258ddf40dc3e448da56
Mostafa Hesham <mostafaasd2018@gmail.com>
2025-02-19 17:58:29 +0200

    add internationalization support to URL patterns

--- CareerShip/urls.py
+++ CareerShip/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
@@ -17,3 +18,5 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += [path('i18n/', include('django.conf.urls.i18n')),]
+urlpatterns += i18n_patterns(path('admin/', admin.site.urls))
