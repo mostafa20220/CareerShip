b12fc61d5f3bd0a00f41b1399778d385d9ebad99
mohamed hesham <scaret2tv@gmail.com>
2025-05-09 00:51:38 +0300

    had to make listing all avaliable team url 'list/'
    it looked ugly one view with the create view 👉🏻👈🏻

--- teams/urls.py
+++ teams/urls.py
@@ -22,9 +22,9 @@ urlpatterns = [
     path('' , views.CreateTeamView.as_view(), name='create_team' ),
     path('leave/' , views.LeaveTeamView.as_view(), name='leave_team' ),
     path("<int:pk>/", views.TeamDetailView.as_view(), name="team-detail"),
+    path('list/', views.ListTeams.as_view(), name='list-create-teams'),
     # invitations URLs
     path("<int:pk>/invite/", views.GenerateInviteView.as_view(), name="generate-invitation"),
     path("invite/<int:pk>/", views.InvitationDetailView.as_view(), name="invitation-detail"),
     path("invite/<int:invite_id>/accept/", views.AcceptInvitationView.as_view(), name="accept-invitation"),
-    path("", views.ListTeams.as_view(), name='list-teams'),
 ]
