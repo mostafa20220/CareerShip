64e554369059394ee2a095a650292a2a322bf92a
mohamed hesham <scaret2tv@gmail.com>
2025-05-08 16:15:08 +0300

    back to the docker database for the moment 👉🏻 👈🏻

--- CareerShip/settings.py
+++ CareerShip/settings.py
@@ -85,19 +85,19 @@ WSGI_APPLICATION = 'CareerShip.wsgi.application'
 
 DATABASES = {
     'default': {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": config("DB_NAME"),
-#         "PASSWORD": config("DB_PASSWORD", default="postgresql"),
-#         "USER": config("DB_USER", default="app"),
-#         "HOST": config("DB_HOST", default="localhost"),
-#         "PORT": config("DB_PORT", default="5432"),
-#         "CONN_MAX_AGE":config("CONN_MAX_AGE", default=60, cast=int),
-        'ENGINE': 'django_cockroachdb',
-        'USER': config("DB_USER"),
-        'PASSWORD': config("DB_PASSWORD"),
-        'HOST': config("DB_HOST"),
-        'PORT': config("DB_PORT"),
-        'NAME': config("DB_NAME"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME"),
+        "PASSWORD": config("DB_PASSWORD", default="postgresql"),
+        "USER": config("DB_USER", default="app"),
+        "HOST": config("DB_HOST", default="localhost"),
+        "PORT": config("DB_PORT", default="5432"),
+        "CONN_MAX_AGE":config("CONN_MAX_AGE", default=60, cast=int),
+#         'ENGINE': 'django_cockroachdb',
+#         'USER': config("DB_USER"),
+#         'PASSWORD': config("DB_PASSWORD"),
+#         'HOST': config("DB_HOST"),
+#         'PORT': config("DB_PORT"),
+#         'NAME': config("DB_NAME"),
 
         'OPTIONS': {
         },
