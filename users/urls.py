a1db0780edea4510a6e202b51eab6e4c63c47acd
Omar <omar1752003@gmail.com>
2025-05-19 08:42:24 +0200

    Add social authentication views and update URL routing
    Included Google and GitHub login views in the users app, and updated the URL patterns to support social authentication through dj-rest-auth.

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,13 @@
+from django.urls import path, include
 
-from django.urls import path
-
-from users.views import LogoutView, RegisterView, ProfileView
+from users.views import (
+    LogoutView,
+    RegisterView,
+    ProfileView,
+    GoogleLoginView,
+    GitHubLoginView,
+    test_social_auth,
+)
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -13,5 +19,10 @@ urlpatterns = [
     path("logout/", LogoutView.as_view(), name="logout"),
     path("login/", TokenObtainPairView.as_view(), name="login"),
     path("refresh/", TokenRefreshView.as_view(), name="refresh"),
-
+    # Social Authentication URLs
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('dj-rest-auth/google/', GoogleLoginView.as_view(), name='google_login'),
+    path('dj-rest-auth/github/', GitHubLoginView.as_view(), name='github_login'),
+    path('test-social-auth/', test_social_auth, name='test_social_auth'),
 ]
