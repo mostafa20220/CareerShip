bfe1fbf5a344dc105ddcd58a7c57fb99126ed29f
mostafah <mostafaasd2018@gmail.com>
2025-06-22 16:38:38 +0300

    Add CSRF_TRUSTED_ORIGINS setting to enhance security

--- CareerShip/settings.py
+++ CareerShip/settings.py
@@ -12,6 +12,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(' ')])
+CSRF_TRUSTED_ORIGINS= config(
+    'CSRF_TRUSTED_ORIGINS',
+    cast=lambda v: [s.strip() for s in v.split(' ')]
+)
 APPEND_SLASH=True
 
 # CELERY SETTINGS
