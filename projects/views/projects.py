dc7b87291957980388f34edeb19802ac6c14e6cd
Omar <omar1752003@gmail.com>
2025-06-24 23:39:15 +0300

    feat: Add filtering options for project listing by difficulty level and category

--- projects/views/projects.py
+++ projects/views/projects.py
@@ -7,10 +7,17 @@ from utils.pagination import StandardPagination
 from django.shortcuts import get_object_or_404
 
 
-
 @api_view(["GET"])
 def list_projects(request):
     projects = Project.objects.all()
+
+    # Filtering by difficulty_level and category (by name)
+    difficulty_level = request.query_params.get("difficulty_level")
+    category = request.query_params.get("category")
+    if difficulty_level:
+        projects = projects.filter(difficulty_level__name=difficulty_level)
+    if category:
+        projects = projects.filter(category__name=category)
 
     paginator = StandardPagination()
     paginated_projects = paginator.paginate_queryset(projects, request)
@@ -21,9 +28,9 @@ def list_projects(request):
 
     return paginated_response
 
+
 @api_view(["GET"])
 def get_project(request, project_id):
     project = get_object_or_404(Project, id=project_id)
     data = ProjectSerializer(project).data
     return Response(data, status=status.HTTP_200_OK)
-
