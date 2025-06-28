48bd87bdb79d411e4fed8fe946d02e6cbbd8b989
mostafah <mostafaasd2018@gmail.com>
2025-06-28 21:47:56 +0300

    FIX REBASE CAUSED PROBLEMSRemove deprecated fields from Team model, add members and owner relationships; create TeamProject model and update submission team reference

--- teams/admin.py
+++ teams/admin.py
@@ -1,41 +1,20 @@
 from django.contrib import admin
-from .models import Team,  TeamUser, Invitation
-
-
-# Inline for displaying team members inside the Team admin page
-class TeamUserInline(admin.TabularInline):  # Use StackedInline for a different layout
-    model = TeamUser
-    extra = 1  # Allows adding one extra inline entry
-
-
-
+from .models import Team, Invitation
 
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
-    list_display = ('name', 'admin', 'is_private', 'created_at')
-    list_filter = ('is_private', 'created_at')
-    search_fields = ('name', 'admin__username')
+    list_display = ('name', 'owner', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('name', 'owner__first_name')
     ordering = ('-created_at',)
     date_hierarchy = 'created_at'
-    inlines = [
-        TeamUserInline,
-
-    ]  # Shows related users and projects inside the team page
-
-
-
-@admin.register(TeamUser)
-class TeamUserAdmin(admin.ModelAdmin):
-    list_display = ('team', 'user')
-    list_filter = ('team', 'user')
-    search_fields = ('team__name', 'user__username')
 
 
 @admin.register(Invitation)
 class InvitationAdmin(admin.ModelAdmin):
     list_display = ('team', 'created_by', 'created_at', 'expires_in_days', 'is_expired')
     list_filter = ('created_at',)
-    search_fields = ('team__name', 'created_by__username')
+    search_fields = ('team__name', 'created_by__first_name', 'created_by__last_name')
     ordering = ('-created_at',)
     date_hierarchy = 'created_at'
 
